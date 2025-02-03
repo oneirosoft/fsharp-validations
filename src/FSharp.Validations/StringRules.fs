40158039a9b0b5084214a64987db6bbf2527bb23
mark-pro <20671988+mark-pro@users.noreply.github.com>
2025-02-02 20:42:49 -0500

    Genesis: Proof of Concept
    Created validation functions to support the creation of rules, rule
    sets, and validations. Validation will either provide a `Succes entity`
    or a `Failure Map<string, string list>`.
    
    Created a sample of what can be done with rules for strings.
    
    Added support to:
    - Create rules that can apply to rule sets (rules applied to properties)
    - Create rule sets which requires the passing of a property get expr
    - Validation result union of Success or Failure
    - Provide boilerplate string rules
    
    __More to come!__

---
+++ src/FSharp.Validations/StringRules.fs
@@ -0,0 +1,24 @@
+module FSharp.Validations.StringRules
+
+open System.Text.RegularExpressions
+
+let notEmpty = rule ((<>) "") (Some "Value cannot be empty")
+let notNull = rule<string> ((<>) null) (Some "Value cannot be null")
+let matches pattern msg = rule<string> (fun value -> Regex.IsMatch(value, pattern)) (msg |> Option.orElse (Some $"Value does not match the pattern {pattern}"))
+let minLength len = rule<string> (fun value -> value.Length >= len) (Some $"Value is too short. Must be less than {len}")
+let maxLength len = rule<string> (fun value -> value.Length <= len) (Some $"Value is too long. Must be less than {len}")
+let lengthBetween min max = rule<string> (fun value -> value.Length >= min && value.Length <= max) (Some $"Value must be between {min} and {max} characters long")
+let email = matches @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$" (Some "Value is not a valid email address")
+let url = matches @"^(https?://)?([\w\-]+)((\.(\w){2,3})+)(/.*)?$" (Some "Value is not a valid URL")
+let phone = matches @"^\+?(\d{1,3})?[-. ]?\(?(\d{3})\)?[-. ]?(\d{3})[-. ]?(\d{4})$" (Some "Value is not a valid phone number")
+let zipCode = matches @"^\d{5}(-\d{4})?$" (Some "Value is not a valid zip code")
+let alpha = matches @"^[a-zA-Z]+$" (Some "Value must contain only letters")
+let alphaNumeric = matches @"^[a-zA-Z0-9]+$" (Some "Value must contain only letters and numbers")
+let numeric = matches @"^\d+$" (Some "Value must contain only numbers")
+let decimal = matches @"^\d+(\.\d+)?$" (Some "Value must be a decimal number")
+let hexadecimal = matches @"^0[xX][0-9a-fA-F]+$" (Some "Value must be a hexadecimal number")
+let base64 = matches @"^([0-9a-zA-Z+/]{4})*([0-9a-zA-Z+/]{2}==|[0-9a-zA-Z+/]{3}=)?$" (Some "Value must be a base64 encoded string")
+let guid = matches @"^(\{{0,1}[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\}{0,1})$" (Some "Value must be a GUID")
+let ipv4 = matches @"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$" (Some "Value must be an IPv4 address")
+let ipv6 = matches @"^([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}$|^([0-9a-fA-F]{1,4}:){1,7}:$|^([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}$|^([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}$|^([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}$|^([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}$|^([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}$|^[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})$|^:((:[0-9a-fA-F]{1,4}){1,7}|:)$" (Some "Value must be an IPv6 address")
+let macAddress = matches @"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$" (Some "Value must be a MAC address")
---
+++ src/FSharp.Validations/Validator.fs
@@ -0,0 +1,65 @@
+﻿[<AutoOpen>]
+module FSharp.Validations.Validator
+
+open System
+open System.Linq.Expressions
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+open Microsoft.FSharp.Linq.RuntimeHelpers.LeafExpressionConverter
+
+type ValidationErr = { property: string; message: string }
+type ValidationResult<'a> =
+    | Success of 'a
+    | Failure of Map<string, string list>
+
+let private createExpression (expr: ('a -> 'b) Expr) =
+    let eval (expr: Expression) =
+        (expr :?> Expression<Func<'a, 'b>>).Compile()
+    
+    let lambda = 
+        <@ Func<'a, 'b> %expr @>
+        |> QuotationToLambdaExpression
+        |> eval
+
+    match expr with
+    | Lambda(_, PropertyGet(_, prop, _)) ->
+        (lambda, prop.Name)
+    | _ -> failwith $"Ensure that the expression is a property access expression (fun foo -> foo.Bar). Found: {expr}"
+
+let rule<'a> (predicate: 'a -> bool) (message: string option) =
+    (predicate, message |> Option.defaultValue "Validation failed")
+
+let ruleFor<'a, 'b> (selector: Expr<'a -> 'b>) (rules: (('b -> bool) * string) list) entity =
+    
+    let selector, propertyName = createExpression selector
+    
+    rules
+    |> List.map (fun (rule, message) -> (entity |> selector.Invoke |> rule, message))
+    |> List.map (function | true, _ -> Ok entity | _, msg -> Error { message = msg; property = propertyName })
+    |> List.fold (fun acc x ->
+        match (x, acc) with
+        | Ok x, Ok _ -> Ok x
+        | Error result, Error lst -> Error (result :: lst)
+        | Error result, Ok _ -> Error [result]
+        | Ok _, Error lst -> Error lst) (Ok entity)
+
+let ruleSet<'a> rules entity =
+    rules
+    |> List.map (fun rule -> rule entity)
+    |> List.fold (fun acc x ->
+        match (x, acc) with
+        | Ok x, Success _ -> Success x
+        | Error problems, Success _ ->
+            problems
+            |> List.groupBy _.property
+            |> List.map (fun (key, value) -> (key, value |> List.map _.message))
+            |> Map.ofList
+            |> Failure
+        | Error problems, Failure map ->
+            problems
+            |> List.groupBy _.property
+            |> List.map (fun (key, value) -> (key, value |> List.map _.message))
+            |> List.fold (fun acc (key, values) ->
+                Map.change key (function | Some x -> Some (x @ values) | _ -> Some values) acc) map
+            |> Failure
+        | Ok _, Failure map -> Failure map) (Success entity)
